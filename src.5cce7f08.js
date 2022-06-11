326156929a096a570a7d3945250c012fb8518048
andrii248 <88237977+andrii248@users.noreply.github.com>
2022-06-11 08:35:40 +0000

    Deploying to gh-pages from @ andrii248/project7@aa60e2949e9e406079ce3c9f9807991d7eb442d5 🚀

--- src.5cce7f08.js
+++ src.5cce7f08.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project7/src.7899a131.js.map
+//# sourceMappingURL=/project7/src.5cce7f08.js.map
